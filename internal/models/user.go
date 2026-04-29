6ca4396d33c4d3d590255bd174b210f657994e11
Srirag Hariharan <sriraghariharan108@gmail.com>
2026-04-29 10:28:32 +0530

    Updated user model with username, password fields, Added validation.

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,6 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	UserId primitive.ObjectID `json:"_id" bson:"_id"`
-	Username string `json:"username" bson:"username"`
+	UserId   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Username string             `json:"username" bson:"username" validate:"required,min=3,max=30"`
+	Email    string             `json:"email" bson:"email" validate:"required,email"`
+	Password string             `json:"-" bson:"password" validate:"required,min=6"`
 }
