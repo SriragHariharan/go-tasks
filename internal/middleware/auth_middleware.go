84cd7e00b9c93136ebd6157847dbd22151b83659
Srirag Hariharan <sriraghariharan108@gmail.com>
2026-04-29 17:03:16 +0530

    Added auth middleware to verify incoming jwt and attach userId to context object.

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/sriraghariharan/gotasks/internal/utils"
+)
+
+type contextKey string
+
+const userIDKey contextKey = "userId"
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "missing Authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		// Expect: "Bearer <token>"
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "invalid Authorization format", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := parts[1]
+
+		// verify token
+		userID, err := utils.VerifyJwt(tokenString)
+		if err != nil {
+			http.Error(w, "invalid or expired token", http.StatusUnauthorized)
+			return
+		}
+
+		// attach userId to request context
+		ctx := context.WithValue(r.Context(), userIDKey, userID)
+
+		// pass to next handler
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
