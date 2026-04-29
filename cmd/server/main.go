d13482aebb3dcd371bf3e495af5e2b11cdbcf977
Srirag Hariharan <sriraghariharan108@gmail.com>
2026-04-29 09:34:00 +0530

    Installed gorilla/mux for routing, air for server restarts and created basic server to serve homepage

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello from gotasks")
+	r := mux.NewRouter()
+    r.HandleFunc("/", homeHandler)
+
+	//server
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Hello welcome to go tasks"))
 }
